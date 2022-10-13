bd449a272576396e7b5513de3d7ef5d8b3f5e456
Mubangizi Moses <moselee10@gmail.com>
2022-10-13 04:09:27 -0700

    2-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include  <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Description: C program that print function followed by a new line using put
- *    * Return: 0 if successful
- *     */
+ * main - Entry point
+ * Description: C program that print function followed by a new line using put
+ * Return: 0 if successful
+ */
 
 int main(void)
 {
