783ca5bff173b5a9c5873178235a848daea7e7d6
Mubangizi Moses <moselee10@gmail.com>
2022-10-13 04:04:52 -0700

    2-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include  <stdio.h>
 
 /**
- *  * main - This uses program prints exactly the string
- *   * "Programming is like building a multilingual puzzle
- *    * followed by a new line.
- *     * Return: always 0 (Success)
- *      */
+ *  * main - Entry point
+ *   * Description: C program that print function followed by a new line using put
+ *    * Return: 0 if successful
+ *     */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
+}
