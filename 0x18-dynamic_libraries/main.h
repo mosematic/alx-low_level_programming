58f09c17f6073ee1069b1d972116892d926e3058
mosematic <moselee10@gmail.com>
2023-01-16 15:58:22 -0800

    dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#include <unistd.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,10 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
